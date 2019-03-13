2874b401acfe7bc5c72d2305b5e1ac023cd5b83f
Daniel Won <dwon@lyft.com>
2019-03-13 13:50:18 -0700

    Added 'bookmark' skeleton api/type/reducer/sagas

---
+++ amundsen_application/static/js/ducks/bookmarks/sagas.ts
@@ -0,0 +1,72 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { SagaIterator } from 'redux-saga';
+
+import {
+  AddBookmark,
+  AddBookmarkRequest,
+  GetBookmarks,
+  GetBookmarksForUser,
+  GetBookmarksForUserRequest,
+  GetBookmarksRequest,
+  RemoveBookmark,
+  RemoveBookmarkRequest
+} from "./types";
+
+import {
+  addBookmark,
+  removeBookmark,
+  getBookmarks,
+  getBookmarksForUser,
+} from "./api/v0";
+
+
+// AddBookmarks
+export function* addBookmarkWorker(action: AddBookmarkRequest): SagaIterator {
+  try {
+    const response = yield call(addBookmark, action);
+  } catch(e) {
+
+  }
+}
+export function* addBookmarkWatcher(): SagaIterator {
+  yield takeEvery(AddBookmark.ACTION , addBookmarkWorker)
+}
+
+
+// RemoveBookmarks
+export function* removeBookmarkWorker(action: RemoveBookmarkRequest): SagaIterator {
+  try {
+    const response = yield call(removeBookmark, action);
+  } catch(e) {
+
+  }
+}
+export function* removeBookmarkWatcher(): SagaIterator {
+  yield takeEvery(RemoveBookmark.ACTION , removeBookmarkWorker)
+}
+
+
+// GetBookmarks
+export function* getBookmarksWorker(action: GetBookmarksRequest): SagaIterator {
+  try {
+    const response = yield call(getBookmarks, action);
+  } catch(e) {
+
+  }
+}
+export function* getBookmarskWatcher(): SagaIterator {
+  yield takeEvery(GetBookmarks.ACTION, getBookmarksWorker)
+}
+
+
+// GetBookmarksForUser
+export function* getBookmarkForUserWorker(action: GetBookmarksForUserRequest): SagaIterator {
+  try {
+    const response = yield call(getBookmarksForUser, action);
+  } catch(e) {
+
+  }
+}
+export function* getBookmarksForUserWatcher(): SagaIterator {
+  yield takeEvery(GetBookmarksForUser.ACTION, getBookmarkForUserWorker)
+}
