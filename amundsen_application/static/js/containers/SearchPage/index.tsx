f3f2557656cec7d21e0441148a3321a24620065a
Daniel <dwon@lyft.com>
2019-03-14 15:10:57 -0700

    Update search to support multiple resource types (#51)
    * Added Tabs to search results
    *Split executeSearch into two actions:
    - `searchAll` will search all resource types. The options allow you to search different page indexes for different resources. This is useful for loading up &selectedTab=users&tabIndex=5, since you don't necessarily want to fetch page 5 for all tabs.
    - `searchResource` will run a search on a single resource type. This is primarily used for search pagination.
    The URL should always reflect the current state of the search and can be refreshed or shared to maintain state.
    
    * Improved state management with window URL and user actions
    (search submit, pagination, tab change)

--- amundsen_application/static/js/containers/SearchPage/index.tsx
+++ amundsen_application/static/js/containers/SearchPage/index.tsx
@@ -2,14 +2,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { GlobalState } from "../../ducks/rootReducer";
-import { executeSearch } from '../../ducks/search/reducer';
+import { searchAll, searchResource } from '../../ducks/search/reducer';
 import { getPopularTables } from '../../ducks/popularTables/reducer';
 
 import SearchPage, { DispatchFromProps, StateFromProps } from '../../components/SearchPage';
 
 export const mapStateToProps = (state: GlobalState) => {
   return {
-    searchTerm: state.search.searchTerm,
+    searchTerm: state.search.search_term,
     popularTables: state.popularTables,
     tables: state.search.tables,
     users: state.search.users,
@@ -18,7 +18,7 @@ export const mapStateToProps = (state: GlobalState) => {
 };
 
 export const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators({ executeSearch, getPopularTables } , dispatch);
+  return bindActionCreators({ searchAll, searchResource, getPopularTables } , dispatch);
 };
 
 export default connect<StateFromProps, DispatchFromProps>(mapStateToProps, mapDispatchToProps)(SearchPage);
