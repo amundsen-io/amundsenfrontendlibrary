08a60496788c159d94e874949ec0d69dce259c3f
Tamika Tannis <ttannis@lyft.com>
2019-05-14 17:21:30 -0700

    Update base_superset_preview_client, doc, and tests.  Pass 'database' in preview data request

--- amundsen_application/base/base_superset_preview_client.py
+++ amundsen_application/base/base_superset_preview_client.py
@@ -1,63 +1,26 @@
 import abc
-import requests
-import uuid
 
 from flask import Response as FlaskResponse, make_response, jsonify
 from http import HTTPStatus
 from requests import Response
-from typing import Dict  # noqa: F401
+from typing import Dict
 
 from amundsen_application.base.base_preview_client import BasePreviewClient
 from amundsen_application.models.preview_data import ColumnItem, PreviewData, PreviewDataSchema
 
-DEFAULT_DATABASE_ID = 1
-DEFAULT_HEADERS = {}
-DEFAULT_QUERY_LIMIT = 50
-DEFAULT_URL = 'http://localhost:8088/superset/sql_json/'
 
-
-class SupersetPreviewClient(BasePreviewClient):
-    def __init__(self,
-                 * ,
-                 database_id: int = DEFAULT_DATABASE_ID,
-                 headers: Dict = DEFAULT_HEADERS,
-                 query_limit: int = DEFAULT_QUERY_LIMIT,
-                 url: str = DEFAULT_URL) -> None:
-        self.database_id = database_id
-        self.headers = headers
-        self.url = url
+class BaseSupersetPreviewClient(BasePreviewClient):
+    @abc.abstractmethod
+    def __init__(self) -> None:
+        self.headers = {}  # type: Dict
 
     @abc.abstractmethod
-    def post_to_sql_json(self, *, request_data: Dict, headers: Dict) -> Response:
+    def post_to_sql_json(self, *, params: Dict, headers: Dict) -> Response:
         """
         Returns the post response from Superset's `sql_json` endpoint
         """
-        return requests.post(self.url, data=request_data, headers=headers)
+        pass  # pragma: no cover
 
-    @abc.abstractmethod
-    def generate_request_sql(self, *, params: Dict) -> str:
-        """
-        Generates the sql statement for the post request to Superset's `sql_json` endpoint
-        """
-        schema = 'main'  # params.get('schema')
-        table_name = 'ab_user'  # params.get('tableName')
-        return 'SELECT * FROM {schema}.{table} LIMIT {limit}'.format(schema=schema, table=table_name, limit=self.query_limit)
-
-    @abc.abstractmethod
-    def generate_request_data(self, *, params: Dict) -> Dict:
-        """
-        Returns a dictionary of data for the post request to Superset's `sql_json` endpoint
-        """
-        request_data = {}
-
-        # Superset's sql_json endpoint requires a unique client_id
-        request_data['client_id'] = uuid.uuid4()
-
-        request_data['database_id'] = self.database_id
-        request_data['sql'] = self.generate_request_sql(params=params)
-        return request_data
-
-    @abc.abstractmethod
     def get_preview_data(self, params: Dict, optionalHeaders: Dict = None) -> FlaskResponse:
         """
         Returns a FlaskResponse object, where the response data represents a json object
@@ -72,17 +35,15 @@ class SupersetPreviewClient(BasePreviewClient):
             if optionalHeaders is not None:
                 headers.update(optionalHeaders)
 
-            # Generate the request data
-            request_data = self.generate_request_data(params=params)
-
             # Request preview data
-            response = self.post_to_sql_json(request_data=request_data, headers=headers)
+            response = self.post_to_sql_json(params=params, headers=headers)
 
-            # Map the results to amundsen_application.models.preview_data.PreviewDataSchema
+            # Verify and return the results
             response_dict = response.json()
             columns = [ColumnItem(c['name'], c['type']) for c in response_dict['columns']]
             preview_data = PreviewData(columns, response_dict['data'])
-            data, errors = PreviewDataSchema().dump(preview_data)
+            data = PreviewDataSchema().dump(preview_data)[0]
+            errors = PreviewDataSchema().load(data)[1]
             if not errors:
                 payload = jsonify({'preview_data': data})
                 return make_response(payload, response.status_code)
