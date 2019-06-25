85afb54ed2d0e113cb112ad362fc2c9ac1e4ed06
Ryan Lieu <rlieu@lyft.com>
2019-06-25 13:23:37 -0700

    added tests for browsepage

---
+++ amundsen_application/static/js/components/BrowsePage/tests/index.spec.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { BrowsePage } from '../';
+
+import BrowseTags from 'components/BrowseTags';
+
+describe('HomePage', () => {
+  const setup = () => {
+    const wrapper = shallow<BrowsePage>(<BrowsePage/>)
+    return { props, wrapper };
+  };
+  
+  let props;
+  let wrapper;
+
+  beforeAll(() => {
+    const setupResult = setup();
+    props = setupResult.props;
+    wrapper = setupResult.wrapper;
+  });
+
+  describe('render', () => {
+    it('contains BrowseTags', () => {
+      expect(wrapper.contains(<BrowseTags />));
+    });
+  });
+});
