2a12031e4f52d1f436c11e61cacb7c2bfbc1d9aa
Daniel Won <dwon@lyft.com>
2019-06-28 11:29:03 -0700

    Refactored searchAll to utilize searchResult API function

--- amundsen_application/static/js/ducks/search/api/v0.ts
+++ amundsen_application/static/js/ducks/search/api/v0.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 import AppConfig from 'config/config';
-import { ResourceType, SearchAllOptions } from 'interfaces';
+import { ResourceType } from 'interfaces';
 
 import { DashboardSearchResults, TableSearchResults, UserSearchResults } from '../types';
 
@@ -16,39 +16,16 @@ interface SearchAPI {
   users?: UserSearchResults;
 };
 
-export function searchAll(options: SearchAllOptions, term: string) {
-  const searchAllResponse = {
-    search_term: term,
-    tables: undefined,
-    users: undefined,
-  };
-
-  const requests = [
-    axios.get(`${BASE_URL}/table?query=${term}&page_index=${options.tableIndex || 0}`)
-      .then((tableResponse: AxiosResponse<SearchAPI>) => {
-        searchAllResponse.tables = tableResponse.data.tables;
-      })
-  ];
-  if (AppConfig.indexUsers.enabled) {
-    requests.push(
-      axios.get(`${BASE_URL}/user?query=${term}&page_index=${options.userIndex || 0}`)
-        .then((userResponse: AxiosResponse<SearchAPI>) => {
-          searchAllResponse.users = userResponse.data.users;
-        })
-    )
-  }
-
-  return axios.all(requests).then(() => {
-    return searchAllResponse
-  });
-};
-
 export function searchResource(pageIndex: number, resource: ResourceType, term: string) {
+  if (resource === ResourceType.dashboard ||
+     (resource === ResourceType.user && !AppConfig.indexUsers.enabled)) {
+    return Promise.resolve({});
+  }
   return axios.get(`${BASE_URL}/${resource}?query=${term}&page_index=${pageIndex}`)
     .then((response: AxiosResponse<SearchAPI>) => {
       const { data } = response;
       const ret = { searchTerm: data.search_term };
-      ['tables', 'users'].forEach((key) => {
+      ['tables', 'users', 'dashboards'].forEach((key) => {
         if (data[key]) {
           ret[key] = data[key];
         }
