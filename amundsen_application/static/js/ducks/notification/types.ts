9831d9360d329efddb7de6aee04d3ecfe07f322e
Ryan Lieu <rlieu@lyft.com>
2019-07-10 11:46:47 -0700

    update types

--- amundsen_application/static/js/ducks/notification/types.ts
+++ amundsen_application/static/js/ducks/notification/types.ts
@@ -1,14 +1,14 @@
 export enum NotificationType {
   ADDED = 'amundsen/notification/OWNER_ADDED',
   REMOVED = 'amundsen/notification/OWNER_REMOVED',
-  EDITED = 'amundsen/notification/TABLE_EDITED',
+  EDITED = 'amundsen/notification/RESOURCE_EDITED',
   REQUESTED = 'amundsen/notification/OWNER_REQUESTED',
 };
 
 export interface SendNotificationOptions {
-  table_name: string,
-  table_url: string,
-  table_requested: boolean,
-  columns_requested: boolean,
-  message?: string,
+  resource_name: string,
+  resource_url: string,
+  description_requested: boolean,
+  fields_requested: boolean,
+  comment?: string,
 };
