1bbda16514a96ab4b2939a510c9a620b0602fa18
Ryan Lieu <rlieu@lyft.com>
2019-07-31 13:34:55 -0700

    removed z-index fixes and add constants

---
+++ amundsen_application/static/js/components/TableDetail/RequestMetadataForm/constants.ts
@@ -0,0 +1,9 @@
+/* TODO: harcoded string that should be translatable/customizable */
+export const TITLE_TEXT = 'Amundsen Resource Request';
+export const FROM = 'From';
+export const TO = 'To';
+export const REQUEST_TYPE = 'Request Type';
+export const TABLE_DESCRIPTION = 'Table Description';
+export const COLUMN_DESCRIPTIONS = 'Column Descriptions';
+export const ADDITIONAL_DETAILS = 'Additional Details';
+export const SEND_BUTTON = 'Send Verification Request'
--- amundsen_application/static/js/components/TableDetail/RequestMetadataForm/index.tsx
+++ amundsen_application/static/js/components/TableDetail/RequestMetadataForm/index.tsx
@@ -6,11 +6,21 @@ import { NotificationType } from 'interfaces/Notifications';
 import { AvatarLabelProps } from 'components/common/AvatarLabel';
 import { GlobalState } from 'ducks/rootReducer';
 import { connect } from 'react-redux';
+import {
+  TITLE_TEXT,
+  FROM,
+  TO,
+  REQUEST_TYPE,
+  TABLE_DESCRIPTION,
+  COLUMN_DESCRIPTIONS,
+  ADDITIONAL_DETAILS,
+  SEND_BUTTON,
+} from './constants'
 
 interface StateFromProps {
   userEmail: string;
   tableName: string;
-  tableOwners: Array<String>;
+  tableOwners: Array<string>;
 }
 
 type RequestMetadataProps = StateFromProps;
@@ -66,29 +76,29 @@ export class RequestMetadataForm extends React.Component<RequestMetadataProps, R
     return (
       <div className={`request-component ${expandedClass}`}>
         <div className="form-section request-header">
-          <h3 className="title">Amundsen Resource Request</h3>
+          <h3 className="title">{TITLE_TEXT}</h3>
           <button type="button" className="btn btn-close" aria-label={"Close"} onClick={this.toggle}/>
         </div>
         <form onSubmit={ this.submitForm } id="RequestForm">
           <div className="form-section">
-            <label>From</label>
+            <label>{FROM}</label>
             <input type="email" name="sender" className="form-control" required={true} value={this.props.userEmail}/>
           </div>
           <div className="form-section">
-            <label>To</label>
+            <label>{TO}</label>
             <input type="email" name="recipients" className="form-control" required={true} multiple={true} defaultValue={this.props.tableOwners.join(",")}/>
           </div>
           <div className="form-section">
-            <label>Request Type</label>
-            <p><input type="checkbox" name="table-description"/> Table Description</p>
-            <p><input type="checkbox" name="column-description"/> Column Descriptions</p>
+            <label>{REQUEST_TYPE}</label>
+            <p><input type="checkbox" name="table-description"/> {TABLE_DESCRIPTION}</p>
+            <p><input type="checkbox" name="column-description"/> {COLUMN_DESCRIPTIONS}</p>
           </div>
           <div className="form-section">
-            <label>Additional Details</label>
+            <label>{ADDITIONAL_DETAILS}</label>
             <textarea className="form-control" name="details" rows={ 8 } maxLength={ 2000 } />
           </div>
           <button className="submit-button" type="submit">
-            Send Verification Request
+            {SEND_BUTTON}
           </button>
         </form>
       </div>
