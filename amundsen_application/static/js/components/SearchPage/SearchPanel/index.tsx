c9ba2b878fbc7e10d5e991d63324a14c62291d7c
Daniel Won <dwon@lyft.com>
2019-08-08 16:52:19 -0700

    Added todo comment to convert search panel to React.SFC

--- amundsen_application/static/js/components/SearchPage/SearchPanel/index.tsx
+++ amundsen_application/static/js/components/SearchPage/SearchPanel/index.tsx
@@ -9,6 +9,7 @@ export interface SearchPanelProps {
   onChange: (tab: ResourceType) => void;
 }
 
+// TODO - Consider converting to React.SFC if logic does not get added
 export default class SearchPanel extends React.Component<SearchPanelProps> {
 
   constructor(props) {
