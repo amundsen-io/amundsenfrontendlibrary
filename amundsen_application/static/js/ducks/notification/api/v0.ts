7bc8ed9874eeff3334565602eb5222a6c112c887
Tamika Tannis <ttannis@lyft.com>
2019-08-29 14:40:13 -0700

    Notifications ducks cleanup + tests (#280)
    * Notifications ducks cleanup + tests
    
    * Fix issues

--- amundsen_application/static/js/ducks/notification/api/v0.ts
+++ amundsen_application/static/js/ducks/notification/api/v0.ts
@@ -4,10 +4,10 @@ import { NotificationType, SendNotificationOptions } from 'interfaces'
 export function sendNotification(recipients: Array<string>, sender: string, notificationType: NotificationType, options?: SendNotificationOptions) {
   return axios({
     data: {
-      recipients,
-      sender,
       notificationType,
       options,
+      recipients,
+      sender,
     },
     method: 'post',
     url: `/api/mail/v0/notification`,
