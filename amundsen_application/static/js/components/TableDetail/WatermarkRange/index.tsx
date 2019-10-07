f0c5e12f6cb5d260bdc748b66a3598cc5692787b
Daniel Won <dwon@lyft.com>
2019-10-07 13:26:50 -0700

    Polish watermark styles

--- amundsen_application/static/js/components/TableDetail/WatermarkRange/index.tsx
+++ amundsen_application/static/js/components/TableDetail/WatermarkRange/index.tsx
@@ -17,31 +17,39 @@ class WatermarkRange extends React.Component<WatermarkRangeProps> {
 
   render() {
     return (
-        <div className="watermark-range">
-          <img className="range" src="/static/images/watermark-range.png"/>
-          { this.getWatermarksLabel() }
-        </div>
-      )
+      <div className="watermark-range body-2">
+        <img className="range-icon" src="/static/images/watermark-range.png"/>
+        { this.getWatermarksLabel() }
+      </div>
+    );
   }
 
   getWatermarksLabel = () => {
     const low = this.props.watermarks.find((wtm) => wtm.watermark_type === "low_watermark");
     const high = this.props.watermarks.find((wtm) => wtm.watermark_type === "high_watermark");
     if (low === undefined && high === undefined) {
-      return "Non Partitioned Table. Data available for all dates."
+      return (
+        <>
+          Non Partitioned Table.<br/>
+          Data available for all dates.
+        </>
+      );
     }
 
     return (
-      <div className="range-dates">
-        <div className="body-2">
-          From: { moment(low.partition_value, "YYYY-MM-DD").format("MMM DD, YYYY") }
+      <>
+        <div className="range-labels">
+          From:
+          <br/>
+          To:
         </div>
-        <div className="body-2">
-          To: { moment(high.partition_value, "YYYY-MM-DD").format("MMM DD, YYYY") }
+        <div className="range-dates">
+          { moment(low.partition_value, "YYYY-MM-DD").format("MMM DD, YYYY") }
+          <br/>
+          { moment(high.partition_value, "YYYY-MM-DD").format("MMM DD, YYYY") }
         </div>
-      </div>
+      </>
     );
-
   }
 }
 
