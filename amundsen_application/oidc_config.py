b10e5a3e6abc69bd088fa475d0bad4de7ef6c4db
Luke Lowery <luke.lowery@gmail.com>
2019-12-02 16:07:33 -0800

    fixing linting issue

--- amundsen_application/oidc_config.py
+++ amundsen_application/oidc_config.py
@@ -1,7 +1,9 @@
+from typing import Dict, Any
+from flask import Flask
 from amundsen_application.config import LocalConfig
 
 
-def get_access_headers(app):
+def get_access_headers(app: Flask) -> Dict:
     """
     Function to retrieve and format the Authorization Headers
     that can be passed to various microservices who are expecting that.
@@ -16,16 +18,17 @@ def get_access_headers(app):
         return None
 
 
-def get_auth_user(app):
+def get_auth_user(app: Flask) -> Any:
     """
     Retrieves the user information from oidc token, and then makes
     a dictionary 'UserInfo' from the token information dictionary.
     We need to convert it to a class in order to use the information
     in the rest of the Amundsen application.
     :param app: The instance of the current app.
-    :return: A class UserInfo
+    :return: A class UserInfo (Note, there isn't a UserInfo class, so we use Any)
     """
     from flask import g
+    app.logger.info(g.oidc_id_token)
     user_info = type('UserInfo', (object,), g.oidc_id_token)
     # noinspection PyUnresolvedReferences
     user_info.user_id = user_info.preferred_username
