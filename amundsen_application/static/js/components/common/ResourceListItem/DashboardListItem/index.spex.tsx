83ac5a3471d4c267a234754d4bb52fa8fff1965c
Tamika Tannis <ttannis@lyft.com>
2020-04-13 10:30:51 -0700

    Some last tests

--- amundsen_application/static/js/components/common/ResourceListItem/DashboardListItem/index.spex.tsx
+++
@@ -1,1 +0,0 @@
-// TODO ttannis: Add tests when we know what this is supposed to render
--- amundsen_application/static/js/components/common/ResourceListItem/DashboardListItem/index.tsx
+++ amundsen_application/static/js/components/common/ResourceListItem/DashboardListItem/index.tsx
@@ -26,7 +26,7 @@ class DashboardListItem extends React.Component<DashboardListItemProps, {}> {
 
   getLink = () => {
     const { dashboard, logging } = this.props;
-    return `/dashboard/${dashboard.uri}`;
+    return `/dashboard/${dashboard.uri}?index=${logging.index}&source=${logging.source}`;
   };
 
   render() {
