c42daaac90883f3109317b3c33b816a18e8a5652
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-05-19 13:57:57 -0700

    Pruning moment locales from the bundle

--- amundsen_application/static/webpack.common.ts
+++ amundsen_application/static/webpack.common.ts
@@ -4,6 +4,7 @@ import * as webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
+import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
 
 import appConfig from './js/config/config';
 
@@ -71,6 +72,7 @@ const config: webpack.Configuration = {
       ],
     },
     plugins: [
+      new MomentLocalesPlugin(),    // To strip all locales except “en”
       new MiniCssExtractPlugin(),
       ...htmlWebpackPluginConfig,
       new BundleAnalyzerPlugin(),
