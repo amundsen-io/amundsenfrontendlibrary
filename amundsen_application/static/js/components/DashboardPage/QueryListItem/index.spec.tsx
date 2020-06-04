d0caebecb7efb79705762627d8a7da664ab6aa6c
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-06-03 20:53:11 -0700

    Mades it a functional component with hooks, added link to dashboard

--- amundsen_application/static/js/components/DashboardPage/QueryListItem/index.spec.tsx
+++ amundsen_application/static/js/components/DashboardPage/QueryListItem/index.spec.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { mount } from 'enzyme';
 
-import QueryListITem, { QueryListItemProps } from './';
+import QueryListItem, { QueryListItemProps } from './';
 
 
 const setup = (propOverrides?: Partial<QueryListItemProps>) => {
@@ -13,7 +13,7 @@ const setup = (propOverrides?: Partial<QueryListItemProps>) => {
     name: "testName",
     ...propOverrides,
   };
-  const wrapper = mount<QueryListITem>(<QueryListITem {...props} />);
+  const wrapper = mount(<QueryListItem {...props} />);
 
   return { props, wrapper };
 };
@@ -21,12 +21,89 @@ const setup = (propOverrides?: Partial<QueryListItemProps>) => {
 describe('QueryListItem', () => {
 
   describe('render', () => {
-
       it('should render without errors', () => {
         expect(() => {
-          const { wrapper } = setup();
+          setup();
         }).not.toThrow();
       });
+
+      it('should render one query list item', () => {
+        const { wrapper} = setup();
+        const expected = 1;
+        const actual = wrapper.find('.query-list-item').length;
+
+        expect(actual).toEqual(expected);
+      });
+
+      it('should render the query name', () => {
+        const { wrapper, props } = setup();
+        const expected = props.name;
+        const actual = wrapper.find('.query-list-item-name').text();
+
+        expect(actual).toEqual(expected);
+      });
+
+      it('should not render the expanded content', () => {
+        const { wrapper } = setup();
+        const expected = 0;
+        const actual = wrapper.find('.query-list-expanded-content').length;
+
+        expect(actual).toEqual(expected);
+      });
+
+      describe('when item is expanded', () => {
+        let wrapper;
+
+        beforeAll(() => {
+          ({ wrapper } = setup());
+
+          wrapper.find('.list-group-item').simulate('click');
+        });
+
+        it('should show the query label', () => {
+          const expected = 1;
+          const actual = wrapper.find('.query-list-query-label').length;
+
+          expect(actual).toEqual(expected);
+        });
+
+        it('should show the query content', () => {
+          const expected = 1;
+          const actual = wrapper.find('.query-list-query-content').length;
+
+          expect(actual).toEqual(expected);
+        });
+      });
+  });
+
+  describe('lifetime', () => {
+
+    describe('when clicked on the item', () => {
+      it('should render the expanded content', () => {
+        const { wrapper, props } = setup();
+        const expected = 1;
+        let actual;
+
+        wrapper.find('.list-group-item').simulate('click');
+        actual = wrapper.find('.query-list-expanded-content').length;
+
+        expect(actual).toEqual(expected);
+      });
+
+      describe('when clicking again', () => {
+        it('should hide the expanded content', () => {
+          const { wrapper, props } = setup();
+          const expected = 0;
+          let actual;
+
+          wrapper.find('.list-group-item').simulate('click');
+          wrapper.find('.list-group-item').simulate('click');
+          actual = wrapper.find('.query-list-expanded-content').length;
+
+          expect(actual).toEqual(expected);
+        });
+      });
+    });
   });
 
 });
--- amundsen_application/static/js/components/DashboardPage/QueryListItem/index.tsx
+++ amundsen_application/static/js/components/DashboardPage/QueryListItem/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import './styles.scss';
+
 export interface QueryListItemProps {
   key: string;
   text: string;
@@ -7,11 +9,29 @@ export interface QueryListItemProps {
   name: string;
 }
 
-class QueryListItem extends React.Component<QueryListItemProps, {}> {
+const QUERY_LABEL = "Query";
 
-  render() {
-    return (<div />);
-  }
+const QueryListItem = ({ name, text, key, url }: QueryListItemProps) => {
+  const [isExpanded, setExpanded] = React.useState(false);
+  const toggleExpand = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <li className="list-group-item query-list-item clickable" onClick={toggleExpand} key={key}>
+      <div className="query-list-header">
+        <p className="query-list-item-name column-name">{ name }</p>
+      </div>
+      {
+        isExpanded &&
+        <div className="query-list-expanded-content">
+          <label className="query-list-query-label section-title" htmlFor="query">{QUERY_LABEL}:</label>
+          <div className="query-list-query-content">{text}</div>
+          <a href={url}>See Dashboard</a>
+        </div>
+      }
+    </li>
+  );
 }
 
 export default QueryListItem;
