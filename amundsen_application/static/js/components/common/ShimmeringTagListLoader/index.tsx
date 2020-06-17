42ca31ccc09b9f1de466f178cc79a311406074e1
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-06-17 15:33:46 -0700

    Setting explicit values for the numItems prop

--- amundsen_application/static/js/components/common/ShimmeringTagListLoader/index.tsx
+++ amundsen_application/static/js/components/common/ShimmeringTagListLoader/index.tsx
@@ -20,7 +20,7 @@ export const ShimmeringTagItem: React.SFC<ShimmeringTagItemProps> = ({
 };
 
 export interface ShimmeringTagListLoaderProps {
-  numItems?: number; // Max items would be 15
+  numItems?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
 }
 
 const ShimmeringTagListLoader: React.SFC<ShimmeringTagListLoaderProps> = ({
