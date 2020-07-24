6c4f11b7eeaee8febc5695437398c384531737e1
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-07-24 11:22:24 -0700

    logic for search first try on badges

--- amundsen_application/static/js/components/Badges/index.tsx
+++ amundsen_application/static/js/components/Badges/index.tsx
@@ -2,14 +2,40 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import Flag, { FlagProps } from 'components/common/Flag';
+import { ResourceType } from 'interfaces';
+import { updateSearchState } from 'ducks/search/reducer';
+import { UpdateSearchStateRequest } from 'ducks/search/types';
+import { logClick } from 'ducks/utilMethods';
 
 import './styles.scss';
 
+export interface DispatchFromProps {
+    searchBadge: (badgeText: string) => UpdateSearchStateRequest;
+}
+
+export type ClickableBadgeProps = DispatchFromProps;
+
 export class ClickableBadge extends React.Component<FlagProps> {
+
+    onClick = (e) => {
+        const badgeText = this.props.text;
+        logClick(e, {
+          target_type: 'badge',
+          label: name,
+        });
+        this.props.searchBadge(badgeText);
+    } 
+
     render() {
         return (
-            <span className='clickable-badge'>
+            <span
+            className='clickable-badge'
+            onClick={this.onClick}
+            >
             <Flag
                 caseType={this.props.caseType}
                 text={this.props.text}
@@ -20,4 +46,23 @@ export class ClickableBadge extends React.Component<FlagProps> {
     }
 }
 
-export default ClickableBadge;
+export const mapDispatchToProps = (dispatch: any) => {
+    return bindActionCreators(
+      {
+        searchBadge: (badgeText: string) =>
+          updateSearchState({
+            filters: {
+              [ResourceType.dashboard]: { badge: badgeText },
+              [ResourceType.table]: { badge: badgeText },
+            },
+            submitSearch: true,
+          }),
+      },
+      dispatch
+    );
+  };
+
+export default connect<null, DispatchFromProps>(
+    null,
+    mapDispatchToProps
+  )(ClickableBadge);
