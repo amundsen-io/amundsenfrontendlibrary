3f3bcd0150c609acaa006fd5313d3367b1620be0
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-08-06 10:04:53 -0700

    Restoring proper announcements endpoint code

--- amundsen_application/api/announcements/v0.py
+++ amundsen_application/api/announcements/v0.py
@@ -28,11 +28,6 @@ announcements_blueprint = Blueprint('announcements', __name__, url_prefix='/api/
 def get_announcements() -> Response:
     global ANNOUNCEMENT_CLIENT_INSTANCE
 
-    # Testing Code, remove at the end
-    payload = jsonify({"msg":"Success","posts":[{"date":"October 31, 2018","html_content":"Tables under the following schemas are now available in Amundsen! \ud83c\udf89\n\n<ul>\n<li>basemap</li>\n<li>driver_engagement</li>\n<li>enterprise</li>\n<li>experimentation</li>\n<li>production</li>\n</ul>\n","title":"New Schemas Added"},{"date":"October 31, 2018","html_content":"<p>Presto views were not being indexed correctly in Amundsen. As of September 27th 2018, we have removed Presto views from Amundsen until we scope out a long term solution. See <a href=\"https://superset.lyft.net/superset/sqllab?id=1638\">this Superset query</a> for the full list of views that were removed.</p>","title":"Presto Views Removed"},{"date":"September 11, 2018","html_content":"<p>The search feature has been modified to work with query terms that include a period, underscore, or empty space. For example, you can now search for <code>core.fact_rides</code> or <code>core fact rides</code> or <code>fact_rides</code>. We now also display paginated results to further aid your search.</p>\n\n<p>Curious to know more about the frequent users of a resource? You can now click on a frequent user to get directed to their Family profile.<p>\n\n<p>Curious to know how a table was generated? We now provide a link to the Airflow DAG.<p>\n\n<p>We have updated the formatting of the url for the table detail page. Please make a note to update any links you may have shared with others or posted in any docs.</p>","title":"Search Improvements & New Features"}]})
-
-    return make_response(payload, HTTPStatus.OK)
-
     try:
         if ANNOUNCEMENT_CLIENT_INSTANCE is None and ANNOUNCEMENT_CLIENT_CLASS is not None:
             ANNOUNCEMENT_CLIENT_INSTANCE = ANNOUNCEMENT_CLIENT_CLASS()
