8b883861432b73ecb19fd9910833499d3fd3f555
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-08-17 12:01:51 -0700

    Testing storybook wiring on dev
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- amundsen_application/api/__init__.py
+++ amundsen_application/api/__init__.py
@@ -11,12 +11,18 @@ ENVIRONMENT = os.getenv('APPLICATION_ENV', 'development')
 
 def init_routes(app: Flask) -> None:
     app.add_url_rule('/healthcheck', 'healthcheck', healthcheck)
+    app.add_url_rule('/storybook', 'storybook', storybook, defaults={'path': ''})
     app.add_url_rule('/', 'index', index, defaults={'path': ''})  # also functions as catch_all
     app.add_url_rule('/<path:path>', 'index', index)  # catch_all
 
 
 def index(path: str) -> Any:
     return render_template("index.html", env=ENVIRONMENT)  # pragma: no cover
+
+
+def storybook(path: str) -> Any:
+    ## TODO: Stuck here, not finding the template
+    return render_template("../storybook-static/templates/index.html", env=ENVIRONMENT)  # pragma: no cover
 
 
 def healthcheck() -> Tuple[str, int]:
