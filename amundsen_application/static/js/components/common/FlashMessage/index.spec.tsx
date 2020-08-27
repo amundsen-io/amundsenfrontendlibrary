fb683dff898ee47aba2cf781b32831e5925d020c
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-08-27 11:42:48 -0700

    Updates FlashMessage test
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- amundsen_application/static/js/components/common/FlashMessage/index.spec.tsx
+++ amundsen_application/static/js/components/common/FlashMessage/index.spec.tsx
@@ -23,15 +23,17 @@ describe('FlashMessage', () => {
   describe('render', () => {
     let props: FlashMessageProps;
     let wrapper;
+
     beforeAll(() => {
-      const setupResult = setup();
-      props = setupResult.props;
-      wrapper = setupResult.wrapper;
+      ({ props, wrapper } = setup());
     });
 
     describe('iconClass logic', () => {
       it('if no iconClass, does not render img', () => {
-        expect(wrapper.find('img').exists()).toBe(false);
+        const expected = 0;
+        const actual = wrapper.find('img').length;
+
+        expect(actual).toEqual(expected);
       });
 
       it('if iconClass, renders img with correct className', () => {
@@ -45,13 +47,17 @@ describe('FlashMessage', () => {
     });
 
     it('renders correct message text', () => {
-      expect(wrapper.find('div.message').text()).toBe(props.message);
+      const expected = props.message;
+      const actual = wrapper.find('.message').text();
+
+      expect(actual).toEqual(expected);
     });
 
     it('renders correct button', () => {
-      expect(wrapper.find('button.btn.btn-close').props().onClick).toBe(
-        props.onClose
-      );
+      const expected = props.onClose;
+      const actual = wrapper.find('button.btn.btn-close').props().onClick;
+
+      expect(actual).toEqual(expected);
     });
   });
 });
