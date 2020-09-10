a3f41229eee77afb7e6c767f1f6d9fca667fab51
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-09-10 09:56:07 -0700

    Up and Down SVG icons
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- amundsen_application/static/js/components/common/SVGIcons/index.tsx
+++ amundsen_application/static/js/components/common/SVGIcons/index.tsx
@@ -2,8 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from './AlertIcon';
+export * from './DownIcon';
+export * from './UpIcon';
 
 export enum IconSizes {
   REGULAR = 24,
   SMALL = 16,
+}
+
+export interface IconProps {
+  stroke?: string;
+  size?: number;
+  fill?: string;
 }
