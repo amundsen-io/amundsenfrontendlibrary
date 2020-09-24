fa07419db893d3229e2b4fb350af868e249fc55a
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-09-24 10:21:36 -0700

    Removing columns
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- amundsen_application/static/js/components/ColumnList/constants.ts
+++ amundsen_application/static/js/components/ColumnList/constants.ts
@@ -2,4 +2,4 @@ export const MORE_BUTTON_TEXT = 'More options';
 export const REQUEST_DESCRIPTION_TEXT = 'Request Column Description';
 export const EMPTY_MESSAGE = 'There are no available columns for this table';
 export const EDITABLE_SECTION_TITLE = 'Description';
-export const COLUMN_STATS_TITLE = 'Column Statistics:';
+export const COLUMN_STATS_TITLE = 'Column Statistics';
