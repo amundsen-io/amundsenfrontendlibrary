6b658c43899b8499eb9de3765bca332c631a4da0
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-10-08 11:23:54 -0700

    fixed method isse
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- amundsen_application/static/js/components/ColumnList/index.tsx
+++ amundsen_application/static/js/components/ColumnList/index.tsx
@@ -288,7 +288,7 @@ const ColumnList: React.FC<ColumnListProps> = ({
     ];
   }
 
-  if (hasColumnWithBadge) {
+  if (hasColumnBadges) {
     formattedColumns = [
       ...formattedColumns,
       {
