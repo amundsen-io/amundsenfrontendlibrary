240bca4f074949cfda94ede64ca417ba5b408c28
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-10-08 12:03:12 -0700

    lint
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- amundsen_application/static/js/components/common/Flag/index.tsx
+++ amundsen_application/static/js/components/common/Flag/index.tsx
@@ -11,6 +11,7 @@ export enum CaseType {
   LOWER_CASE = 'lowerCase',
   SENTENCE_CASE = 'sentenceCase',
   UPPER_CASE = 'upperCase',
+  TITLE_CASE = 'titleCase',
 }
 
 export interface FlagProps {
@@ -27,6 +28,13 @@ export function convertText(str = '', caseType: string): string {
       return `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`;
     case CaseType.UPPER_CASE:
       return str.toUpperCase();
+    case CaseType.TITLE_CASE:
+      const splitStr = str.toLowerCase().split(' ');
+      for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] =
+          splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+      }
+      return splitStr.join(' ');
     default:
       return str;
   }
