22d9d8757caa641206e94e2ac96c3bc3659b4fad
Dorian Johnson <2020@dorianj.net>
2020-11-30 16:37:43 -0800

    chore: fix test failure
    This error was introduced in https://github.com/amundsen-io/amundsenfrontendlibrary/commit/0b47694ea74cbbef34e03eb45f29643b16a1332a
    
    I do not understand why that changeset passed CI (it shouldn't have), but
    once it landed into master it didn't. But regardless, fix
    
    Signed-off-by: Dorian Johnson <2020@dorianj.net>

--- amundsen_application/api/utils/metadata_utils.py
+++ amundsen_application/api/utils/metadata_utils.py
@@ -109,7 +109,8 @@ def marshall_table_full(table_dict: Dict) -> Dict:
     table: Table = schema.load(table_dict).data
     results: Dict[str, Any] = schema.dump(table).data
 
-    results['is_editable'] = is_table_editable(results['schema'], results['name'])
+    is_editable = is_table_editable(results['schema'], results['name'])
+    results['is_editable'] = is_editable
 
     # TODO - Cleanup https://github.com/lyft/amundsen/issues/296
     #  This code will try to supplement some missing data since the data here is incomplete.
