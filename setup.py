f41b1d74fb48487e4a4dbfec05b5fe76f93be4af
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-01-12 11:36:47 -0800

    moved pyarrow toe xtras require
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ setup(
     setup_requires=['cython >= 0.29'],
     install_requires=requirements,
     extras_require={
-        'oidc': ['flaskoidc==0.1.1']
+        'oidc': ['flaskoidc==0.1.1'],
+        'pyarrow': ['pyarrow==2.0.0'],
     },
     python_requires=">=3.6",
     entry_points="""
