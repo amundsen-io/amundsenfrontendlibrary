a1b05b35b9ee1d57474602ead15059474d08d55e
Shaheen Lavie-Rouse <shaheen.lavie.rouse@gmail.com>
2021-04-06 17:38:59 -0700

    fix lint errors
    Signed-off-by: Shaheen Lavie-Rouse <shaheen.lavie.rouse@gmail.com>

--- amundsen_application/static/js/components/EditableText/index.tsx
+++ amundsen_application/static/js/components/EditableText/index.tsx
@@ -125,7 +125,7 @@ class EditableText extends React.Component<
       return (
         <div className="editable-text">
           <div className="markdown-wrapper">
-            <ReactMarkdown allowDangerousHtml={true}>{value}</ReactMarkdown>
+            <ReactMarkdown allowDangerousHtml>{value}</ReactMarkdown>
           </div>
           {editable && !value && (
             <a
