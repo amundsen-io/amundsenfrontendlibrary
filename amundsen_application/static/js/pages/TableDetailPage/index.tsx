c9f71f91a9e17c45edd6039adb1eaedb546d0f9b
Grant Seward <grant@stemma.ai>
2021-04-15 15:13:23 -0400

    Removed check to display watermark only if table is not a view
    Signed-off-by: Grant Seward <grant@stemma.ai>

--- amundsen_application/static/js/pages/TableDetailPage/index.tsx
+++ amundsen_application/static/js/pages/TableDetailPage/index.tsx
@@ -423,14 +423,14 @@ export class TableDetail extends React.Component<
                       </time>
                     </section>
                   )}
-                  {!data.is_view && (
+                  {
                     <section className="metadata-section">
                       <div className="section-title">
                         {Constants.DATE_RANGE_TITLE}
                       </div>
                       <WatermarkLabel watermarks={data.watermarks} />
                     </section>
-                  )}
+                  }
                   <EditableSection title={Constants.TAG_TITLE}>
                     <TagInput
                       resourceType={ResourceType.table}
